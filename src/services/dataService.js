0050ac89b74565b492a778ba953f96b1d20207b2
rocabee-david <david@rocabee.com>
2017-08-29 22:03:42 +0100

    udpate

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,26 +1,26 @@
 'use strict';
 
 angular.module('currencyExchange').service('dataService', function() {
-	return {
-		currencies: {},
-		base: 'USD',
-		rates: {},
-		baseTotal: '',
-		transfered: 'AED',
-		trasferTotal: '',
-		
-		calculateTransferedTotal: function() {
-			const absoluteTotal = parseFloat((Math.abs(this.baseTotal) * this.rates[this.transfered]).toFixed(2))
-			
-			if (isNaN(absoluteTotal)) {
-				return '';
-			}
+    return {
+        currencies: {},
+        base: 'USD',
+        rates: {},
+        baseTotal: '',
+        transfered: 'AED',
+        trasferTotal: '',
+        
+        calculateTransferedTotal: function() {
+            const absoluteTotal = parseFloat((Math.abs(this.baseTotal) * this.rates[this.transfered]).toFixed(2))
+            
+            if (isNaN(absoluteTotal)) {
+                return '';
+            }
 
-			return '+' + absoluteTotal;
-		},
-		
-		calculateBaseTotal: function() {
-			return -parseFloat((Math.abs(this.trasferTotal) / this.rates[this.transfered]).toFixed(2));
-		}
-	}	
+            return '+' + absoluteTotal;
+        },
+        
+        calculateBaseTotal: function() {
+            return -parseFloat((Math.abs(this.trasferTotal) / this.rates[this.transfered]).toFixed(2));
+        }
+    }   
 })
